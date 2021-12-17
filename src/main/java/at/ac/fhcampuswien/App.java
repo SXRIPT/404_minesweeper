4233ba38a7b5b99c115977b61ab0d1b56797c513
Johannes <johannes.dittmoser-pfeifer@stud.fh-campuswien.ac.at>
2021-12-17 16:30:35 +0100

    Feld wird erstellt, befüllt und berrechnet. Funktion showBoard ist nur zur Überprüfung gedacht

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,13 +6,16 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 
-public class App extends Application {
+public class App /*extends Application */{
     public static void main(String[] args){
-        launch(args);
+
+        Board board = new Board();
+        board.showBoard();
+        //launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    //   @Override
+/*    public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Hello JavaFX!");
@@ -21,5 +24,5 @@ public class App extends Application {
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 150));
         primaryStage.show();
-    }
+    }*/
 }
--- src/main/java/at/ac/fhcampuswien/Board.java
+++ src/main/java/at/ac/fhcampuswien/Board.java
@@ -6,8 +6,11 @@ public class Board {
     private int[][] board; // sollte mit eine Konstanten Klasse ergänzt/geändert
     private final int MAX = 16;
     private final int MIN = 0;
+
     Board() { // Konstruktor ohne Parameter
-        this.board = new int[16][16]; //
+        this.board = new int[MAX][MAX]; //
+        randomBombs();
+        calculateBoard();
     }
 
     /*
@@ -20,14 +23,14 @@ public class Board {
         i = 1 => row = 0, col = 0 => board[0][0]
         i = 2 => row = 1; col = 1=> board[1][1]
      */
-    public void randomBombs() {
+    private void randomBombs() {
         Random rand = new Random();
         int i = 0; // Anzahl der Bomben
         while(i < 40) {
             // row random
-            int row = rand.nextInt((MAX - MIN) + 1) + MIN; //
+            int row = rand.nextInt(MAX); //
             // col random
-            int col = rand.nextInt((MAX - MIN) + 1) + MIN; //
+            int col = rand.nextInt(MAX); //
 
             if (board[row][col] != 9) { // falls es sich eine Zahl wiederholt, werden trotzdem 40 Zahlen
                 board[row][col] = 9;
@@ -36,11 +39,49 @@ public class Board {
         }
     }
 
+    private void calculateBoard(){
+
+        for(int i = 0; i < MAX ; i++){
+            for(int j = 0; j < MAX; j++){
+                board[i][j] = checkSurroundings(i,j);
+            }
+        }
+
+
+    }
+    private int checkSurroundings(int i, int j){                //Überprüft die anliegenden Felder auf Bomben
+        int count = 0;
+        if(board[i][j] == 9){                                   //Falls das Feld selber ein Bombe ist soll sie natürlich nicht überschrieben werden
+            count = 9;
+        }
+        else {
+            for (int x = i - 1; x <= i + 1; x++) {
+                for (int y = j - 1; y <= j + 1; y++) {
+                    if (x >= 0  && x <= 15 && y >= 0 && y <=15 && board[x][y] == 9) {             //1.Teil stellt sicher das nicht außerhalb der Spielfelds geschaut wird, der 2.Teil ob es eine Bombe ist
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
     public int[][] getBoard() {
         return board;
     }
 
     public void setBoard(int[][] board) {
         this.board = board;
+    }
+
+    public void showBoard(){                                        //zeigt den befüllten Array in der Konsole für Probe Zwecke
+        for(int i = 0; i < MAX;i++){
+            for(int j = 0; j < MAX;j++){
+                System.out.print(board[i][j] + "  ");
+                if((j + 1)  % 16 == 0){
+                    System.out.println("");
+                }
+            }
+        }
     }
 }
